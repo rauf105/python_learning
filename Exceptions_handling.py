c1e9129017f0a186abf9efb890c2a12d375ae083
rauf105 <abdurraufrx@gmail.com>
2026-02-09 23:09:43 +0600

    learn about error handling
    here i use try, except, else, pass, break

---
+++ Exceptions_handling.py
@@ -0,0 +1,17 @@
+def main():
+    x=get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("what is x? "))
+
+        except ValueError:
+            print(f"x in not a intger")
+            
+        else:   #after try else will run. 
+            break
+    return x
+
+main()
