9776e84a1d1e6278e0978a2b9fc5c652793becf2
rauf105 <abdurraufrx@gmail.com>
2026-02-09 21:36:01 +0600

    practice nested for loop to make 2d block

---
+++ nested_forloop.py
@@ -0,0 +1,11 @@
+def main():
+    print_squar(3)
+
+def print_squar(size):
+    for i in range(size):
+        for j in range(size):
+            print("#",end="")
+            #print("#" *size) #string multiplecation
+        print()
+
+main()
