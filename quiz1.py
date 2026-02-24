e990609d5e9f65d3ae01b13aad07c9c293475b81
rauf105 <abdurraufrx@gmail.com>
2026-02-24 21:04:25 +0600

    Create quiz1.py

---
+++ quiz1.py
@@ -0,0 +1,22 @@
+# x, y, z = "hel", "lo wo","rld"
+# print(x)
+# print(y)
+# print(z)
+
+# x = 'awesome'
+# def myfunc():
+#   global x
+#   x = 'fantastic'
+# myfunc()
+# print('Python is ' + x)
+
+# carname = "Volvo"
+# print(carname)
+
+# txt = "Hello World"
+# print(txt[0])
+
+a = 330
+b = 330
+
+print("A") if a > b else print("=") if a == b else print("B")
