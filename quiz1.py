b114c4feca7be23bf3cb08914b9e1d8080f3990a
rauf105 <abdurraufrx@gmail.com>
2026-02-25 07:05:09 +0600

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -20,11 +20,28 @@
 # b = 330
 # print("A") if a > b else print("=") if a == b else print("B")
 
-day = "bal"
-match day:
-  case "bal":
-    print("Today is Saturday")
-  case 7:
-    print("Today is Sunday")
-  case _:
-    print("Looking forward to the Weekend")
+# day = "bal"
+# match day:
+#   case "bal":
+#     print("Today is Saturday")
+#   case 7:
+#     print("Today is Sunday")
+#   case _:
+#     print("Looking forward to the Weekend")
+
+# i=1
+# while i<10:
+#     i += 1
+    
+#     if i==5:
+#         #break
+#         continue
+#     print(i-1)
+# else: print("code no longer less than 10")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
