3d2d8cf80a005042e9504acad8883421a03d5154
rauf105 <abdurraufrx@gmail.com>
2026-04-05 13:21:07 +0600

    Create Doremy's Paint 3.py

---
+++ Doremy's Paint 3.py
@@ -0,0 +1,20 @@
+from collections import Counter
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    freq = Counter(a)
+    if len(freq) == 1:
+        print("Yes")
+    
+    elif len(freq) == 2:
+        value = list(freq.values())
+        if abs(value[0] - value[1]) <= 1:
+            print("Yes")
+        else:
+            print ("No")
+    else:
+         print ("No")
+
