5edb6d797dc71a4ef3ed6e412d1f5ce1dbf54d79
rauf105 <abdurraufrx@gmail.com>
2026-06-28 15:30:32 +0600

    iuhu6fgu

---
+++ labtask.py
@@ -0,0 +1,9 @@
+for a in range(1, 1000):
+    for b in range(a + 1, 1000):
+        c = 1000 - a - b
+
+        if a*a + b*b == c*c:
+            print("a =", a)
+            print("b =", b)
+            print("c =", c)
+            
